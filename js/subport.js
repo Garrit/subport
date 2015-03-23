9e34ac68d8ccb0935ec2c390e024e8aef2689b3f
Samuel Coleman <samuel@seenet.ca>
2015-03-23 17:51:01 -0300

    Add frontend capable of browsing problems.

---
+++ js/subport.js
@@ -0,0 +1,48 @@
+Handlebars.registerHelper('marked', function (contents) {
+    return new Handlebars.SafeString(marked(contents));
+});
+
+var setActiveMenuItem = function (item) {
+    $('li.active').removeClass('active');
+    $('#' + item + '-menu-item').addClass('active');
+}
+
+var renderTemplate = function (template, contents, callback) {
+    $.get(template + '.html', function (source) {
+        template = Handlebars.compile(source);
+        callback(template(contents));
+    })
+};
+
+routie({
+    '/problems': function () {
+        setActiveMenuItem('problems');
+
+        $.getJSON('backend/problems', function (problems) {
+            contents = {problems: problems};
+            renderTemplate('problems', contents, function (template) {
+                $('#main').html(template);
+            });
+        });
+    },
+    '/problems/:name': function (name) {
+
+        $.getJSON('backend/problems/' + name, function (problem) {
+            renderTemplate('problem', problem, function (template) {
+                $('#main').html(template);
+            });
+        });
+    },
+    '/submissions': function () {
+        setActiveMenuItem('submissions');
+
+        renderTemplate('submissions', {}, function (template) {
+            $('#main').html(template);
+        });
+    }
+});
+
+$(document).ready(function () {
+    if (window.location.hash.length == 0)
+        routie('/problems');
+});
