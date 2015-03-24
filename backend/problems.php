da53c0ac9e025cb964a08075b9c65bdf5d4a25d4
Samuel Coleman <samuel@seenet.ca>
2015-03-23 22:42:42 -0300

    Update problem parsing for changed property names.

--- backend/problems.php
+++ backend/problems.php
@@ -5,9 +5,9 @@ function read_problem($problem_name)
     $problem_file = PROBLEMS_PATH . '/' . $problem_name . '/problem.json';
     $problem = json_decode(file_get_contents($problem_file));
 
-    if (isset($problem->description_file))
+    if (isset($problem->descriptionFile))
     {
-        $description_file = dirname($problem_file) . '/' . $problem->description_file;
+        $description_file = dirname($problem_file) . '/' . $problem->descriptionFile;
         $problem->description = file_get_contents($description_file);
     }
 
